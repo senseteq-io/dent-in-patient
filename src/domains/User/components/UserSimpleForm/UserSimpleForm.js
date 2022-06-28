602471c40cbf5d8e7297cf5b57b4a023ba82f04c
sashka2131 <uhucbeucg@gmail.com>
2022-06-28 14:28:24 +0200

    Add user simple form where user pass his ssn and also can change his name and surname from vipps

---
+++ src/domains/User/components/UserSimpleForm/UserSimpleForm.js
@@ -0,0 +1,119 @@
+import { Form, Button, Input, notification } from 'antd'
+import PropTypes from 'prop-types'
+import { useTranslations } from 'contexts/Translation'
+import { useHistory } from 'react-router-dom'
+import { useUserFormValidators } from 'domains/User/hooks'
+import { updateVippsBookingFromWidget } from 'domains/Booking/helpers'
+
+const PROD_API_URL = process.env.REACT_APP_PROD_API_URL
+
+const UserSimpleForm = (props) => {
+  const { initialValues } = props
+
+  const history = useHistory()
+  const [form] = Form.useForm()
+  const { t } = useTranslations()
+  const { postalCodeValidator, personalNumberValidator } =
+    useUserFormValidators()
+
+  const onSubmitUser = async (userData) => {
+    const userPhoneFormatted = initialValues?.phoneNumber
+      ? `+${initialValues?.phoneNumber}`
+      : null
+    const userDataToUpdate = {
+      firstName: userData?.firstName,
+      lastName: userData?.lastName,
+      postalCode: userData?.postalCode,
+      phone: userPhoneFormatted,
+      nin: userData?.personalNumber
+    }
+
+    try {
+      // update user using our backend API
+      await fetch(PROD_API_URL + `/user/${initialValues?._id}`, {
+        method: 'PATCH',
+        cache: 'no-cache',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(userDataToUpdate)
+      })
+      // update pending booking from widget
+      updateVippsBookingFromWidget({
+        pendingBookingId: initialValues?.bookingId,
+        clientPhone: userPhoneFormatted,
+        userId: initialValues?._id
+      })
+
+      history.push('/bookings')
+    } catch (e) {
+      console.error('Error occurred during saving profile data. ', e.message)
+      notification.error({
+        message: t('Failed to save profile data'),
+        description: e.message
+      })
+    }
+  }
+
+  return (
+    <Form
+      form={form}
+      layout="vertical"
+      initialValues={initialValues}
+      onFinish={onSubmitUser}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="First Name"
+        name="firstName"
+        rules={[{ required: true, message: t('Please enter your name!') }]}
+      >
+        <Input autoFocus placeholder={t('Please enter your name')} />
+      </Form.Item>
+      <Form.Item
+        label="Last Name"
+        name="lastName"
+        rules={[{ required: true, message: t('Please enter your last name!') }]}
+      >
+        <Input placeholder={t('Please enter your last name')} />
+      </Form.Item>
+      <Form.Item
+        label="Postal Code"
+        name="postalCode"
+        rules={[
+          { required: true, message: t('Please enter your postal code!') },
+          { validator: postalCodeValidator }
+        ]}
+      >
+        <Input
+          placeholder={t('Please enter your postal code')}
+          maxLength={4}
+          minLength={4}
+        />
+      </Form.Item>
+      <Form.Item
+        label="Personal Number"
+        name="personalNumber"
+        rules={[
+          { required: true, message: t('Please enter your personal number!') },
+          { validator: personalNumberValidator }
+        ]}
+      >
+        <Input
+          placeholder={t('Please enter your personal number')}
+          maxLength={11}
+        />
+      </Form.Item>
+
+      <Button type="primary" htmlType="submit" block>
+        {t('Submit')}
+      </Button>
+    </Form>
+  )
+}
+
+UserSimpleForm.propTypes = {
+  initialValues: PropTypes.object
+}
+
+export default UserSimpleForm
--- src/domains/User/components/index.js
+++ src/domains/User/components/index.js
@@ -1,2 +1,3 @@
 export { default as UserAdvancedForm } from './UserAdvancedForm'
 export { default as UserAdvancedView } from './UserAdvancedView'
+export { default as UserSimpleForm } from './UserSimpleForm'
