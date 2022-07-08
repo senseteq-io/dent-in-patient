15738f1fe706ccd0222bfc25deb4c788aa3ce3e9
sashka2131 <uhucbeucg@gmail.com>
2022-07-09 01:05:45 +0200

    [VIPPS] [SWE-643] Made hook for Vipps background actions like vipps authorize and booking update after vipps login if it necessary #feature #wip

--- src/domains/Session/hooks/index.js
+++ src/domains/Session/hooks/index.js
@@ -1,3 +1,4 @@
 export { default as useSessionActions } from './useSessionActions'
 export { default as useLastSession } from './useLastSession'
 export { default as useGDPRStatus } from './useGDPRStatus'
+export { default as useVippsFlowBackgroundActions } from './useVippsFlowBackgroundActions'
---
+++ src/domains/Session/hooks/useVippsFlowBackgroundActions/useVippsFlowBackgroundActions.js
@@ -0,0 +1,69 @@
+import { useEffect, useMemo, useState } from 'react'
+import { useHistory, useLocation } from 'react-router-dom'
+
+import PATHS from 'pages/paths'
+import { notification } from 'antd'
+import { updateVippsBookingFromWidget } from 'domains/Booking/helpers'
+import { urlParamsToObject } from 'utils'
+import { useUser } from 'domains/User/context'
+import { useVippsLogin } from 'hooks'
+
+const { LOGIN } = PATHS.UNAUTHENTICATED
+
+const useVippsFlowBackgroundActions = () => {
+  const history = useHistory()
+  const location = useLocation()
+  const vippsLogin = useVippsLogin()
+  const { user } = useUser()
+
+  // [COMPONENT_STATE_HOOKS]
+  const [isLoginCalled, setIsLoginCalled] = useState(false)
+  const [dataFromVipps, setDataFromVipps] = useState(null)
+
+  // Get Vipps parameters from url
+  const urlParamsObject = useMemo(
+    () => urlParamsToObject(location.search),
+    [location.search]
+  )
+
+  // Vipps login
+  useEffect(() => {
+    const processVippsLogin = async () => {
+      setIsLoginCalled(true)
+      // Get user info from vipps, authorize to app if successfully get info
+      // and save info to state for next steps
+      const _userAuthInfo = await vippsLogin(urlParamsObject)
+      setDataFromVipps(_userAuthInfo)
+    }
+    if (!isLoginCalled && urlParamsObject?.code) {
+      processVippsLogin()
+    }
+  }, [isLoginCalled, urlParamsObject, vippsLogin])
+
+  useEffect(() => {
+    const processVippsBooking = async () => {
+      try {
+        // If user booked appointment from vipps in widget, update booking with user info
+        await updateVippsBookingFromWidget({
+          pendingBookingId: dataFromVipps.bookingId,
+          clientPhone: dataFromVipps.phoneNumber,
+          userId: user._id
+        })
+        history.push(LOGIN)
+      } catch (e) {
+        notification.error({
+          message: 'Error',
+          description: `Failed to update booking. ${e.message}`
+        })
+        console.error(e)
+      }
+    }
+
+    if (user?.data && dataFromVipps?.bookingId && !dataFromVipps?.isAuth) {
+      processVippsBooking()
+    }
+  }, [dataFromVipps, history, user])
+
+  return dataFromVipps
+}
+export default useVippsFlowBackgroundActions
