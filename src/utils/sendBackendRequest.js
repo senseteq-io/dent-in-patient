7103e5ea2ebbc70f5b8213608316381a657d5686
sashka2131 <uhucbeucg@gmail.com>
2022-07-11 12:48:27 +0200

    [UTILS EXTEND] [SWE-643] Add universal function that help send backend request. #feature #done

---
+++ src/utils/sendBackendRequest.js
@@ -0,0 +1,46 @@
+import { notification } from 'antd'
+
+const PROD_API_URL = process.env.REACT_APP_PROD_API_URL
+
+const sendBackendRequest = async (
+  endpoint,
+  method = 'POST',
+  body,
+  headers,
+  errorDescription,
+  isJsonResponse = true
+) => {
+  try {
+    if (!body || !endpoint) {
+      throw new Error('Missing required parameters as endpoint or body')
+    }
+
+    const jsonBody = JSON.stringify(body)
+
+    // Make api call with passed params
+    const response = await fetch(PROD_API_URL + endpoint, {
+      method,
+      cache: 'no-cache',
+      headers: {
+        'Content-Type': 'application/json',
+        ...headers
+      },
+      body: jsonBody
+    })
+
+    // Return parsed json response if needed
+    if (isJsonResponse) {
+      const jsonResponse = await response.json()
+      return jsonResponse
+    }
+    return response
+  } catch (error) {
+    notification.error({
+      message: 'Error',
+      description: `${errorDescription}. ${error.message}`,
+      placement: 'topRight'
+    })
+  }
+}
+
+export default sendBackendRequest
