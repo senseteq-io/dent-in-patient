046af4cf0bd62344904e81323a18a41e47f0c106
sashka2131 <uhucbeucg@gmail.com>
2022-08-03 16:34:03 +0300

    [TRANSLATIONS LOADING] [SWE-1881] Fix spinner position for translations provider loading state, fix text displaying #bugfix #done

--- src/contexts/Translation/TranslationsProvider.js
+++ src/contexts/Translation/TranslationsProvider.js
@@ -1,3 +1,4 @@
+import { Box, Spin, Text } from '@qonsoll/react-design'
 import {
   CURRENT_APP,
   DEFAULT_LANGUAGE,
@@ -6,11 +7,21 @@ import {
 } from '../Translation/__constants__'
 import Provider, { TranslationContext } from '@qonsoll/translation'
 
-import { Box } from '@qonsoll/react-design'
 import PropTypes from 'prop-types'
-import { Spinner } from 'components'
 import firebase from 'firebase/compat/app'
 import { useMemo } from 'react'
+
+const spinnerWrapperProps = {
+  position: 'fixed',
+  bg: 'white',
+  width: '100%',
+  height: '100%',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  flexDirection: 'column',
+  zIndex: 1000
+}
 
 const TranslationsProvider = (props) => {
   const { children } = props
@@ -33,14 +44,9 @@ const TranslationsProvider = (props) => {
         {({ loaded }) => (
           <Box width="inherit" height="inherit">
             {!loaded && (
-              <Box
-                position="fixed"
-                bg="white"
-                width="100vw"
-                height="100vh"
-                zIndex={1000}
-              >
-                <Spinner text={loadingText} />
+              <Box {...spinnerWrapperProps}>
+                <Spin />
+                <Text>{loadingText}</Text>
               </Box>
             )}
             {children}
